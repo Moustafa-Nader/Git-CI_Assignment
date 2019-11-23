7d6ba53e6889a3ee51e3ce86cebab056e5181b98
Moustafa-Nader <moustafa.mohamed7979@gmail.com>
2019-11-24 01:45:41 +0200

    made CircleCircumference class

--- src/main/java/myPackage/CircleCircumference.java
+++ src/main/java/myPackage/CircleCircumference.java
@@ -1,13 +1,41 @@
 package myPackage;
 public class CircleCircumference extends Thread implements ISubscriber{
+Subject subject;
+public Object obj = new Object();
+
+@Override
+public void Start(){
+    start();
+}
+public void Notify(){
+    synchronized(obj){
+        obj.notify();
+    }
+
+}
+public void setSubject(Subject subject){
+    this.subject = subject;
+}
+public static double getCircleCircumference(double num){
+    return (2*3.1415*num);
+}
+public void Operate(){
+    System.out.println("Circle Circumference = " + getCircleCircumference(subject.getValue()));
+}
+
+@Override
+public void run(){
+try {
+    synchronized(obj){
+        obj.wait();
+    }
+}catch(InterruptedException e){
+    e.printStackTrace();
+}
+
+Operate();
+}
 
 
 
-
-
-
-
-
-
-    
 }
--- src/main/java/myPackage/ISubscriber.java
+++ src/main/java/myPackage/ISubscriber.java
@@ -3,6 +3,6 @@ public interface ISubscriber{
     public void Start();
     public void setSubject(Subject subject);
     public void Notify();
-    public void Calculate();
+    public void Operate();
 
 }
