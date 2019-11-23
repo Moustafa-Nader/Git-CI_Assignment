309f078097574df0d23e56f7749e6f08c9f99b0e
Moustafa-Nader <moustafa.mohamed7979@gmail.com>
2019-11-24 01:31:15 +0200

    made Subject and ISubscriber Classes

--- src/main/java/myPackage/ISubscriber.java
+++ src/main/java/myPackage/ISubscriber.java
@@ -1,7 +1,8 @@
 package myPackage;
 public interface ISubscriber{
-
-
-
+    public void Start();
+    public void setSubject(Subject subject);
+    public void Notify();
+    public void Calculate();
 
 }
--- src/main/java/myPackage/Subject.java
+++ src/main/java/myPackage/Subject.java
@@ -1,8 +1,31 @@
 package myPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subject{
+private List<ISubscriber> subscribers;
+public double value;
 
+public Subject(){
+    subscribers = new ArrayList<ISubscriber>();
+}
+public void addSubscriber(ISubscriber sub){
+    subscribers.add(sub);
+}
+public double getValue(){
+    return this.value;
+}
+public void setValue(double value){
+    this.value = value;
+    updateSubscribers();
+}
+public List<ISubscriber> getSubscribers(){
+    return subscribers;
+}
+public void updateSubscribers(){
+    for(ISubscriber sub : this.getSubscribers())
+    sub.Notify();
+}
 
-
-
-    
 }
