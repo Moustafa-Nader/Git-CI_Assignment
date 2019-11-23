c7eebfff0c92c342806cd5c3fc908dd206f5e4a7
Moustafa-Nader <moustafa.mohamed7979@gmail.com>
2019-11-24 01:54:28 +0200

    made the main(App.java) with the circle subscriber

--- src/main/java/myPackage/App.java
+++ src/main/java/myPackage/App.java
@@ -1,9 +1,25 @@
 package myPackage;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Subject mySubject = new Subject();
+        for(ISubscriber sub : subscribers){
+            sub.setSubject(mySubject);
+            mySubject.addSubscriber(sub);
+            sub.Start();
+        }
+        Scanner scanner = new Scanner(System.in);
+        Double Number = scanner.nextDouble();
+        mySubject.setValue(Number);
+        scanner.close();
     }
+    private static ISubscriber subscribers[] = {
+        new CircleCircumference(),
+    // New functions will be added here
+    };
 }
+
