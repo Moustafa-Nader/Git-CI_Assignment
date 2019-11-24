2b4aac6b000e76f3a852625d1ab220a4b769a650
Moustafa-Nader <moustafa.mohamed7979@gmail.com>
2019-11-24 02:00:08 +0200

    Added travis.yml and made the unit testing of CircleCircumference

--- src/test/java/myPackage/AppTest.java
+++ src/test/java/myPackage/AppTest.java
@@ -1,5 +1,6 @@
 package myPackage;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,10 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void TestCircleCircumference()
     {
-        assertTrue( true );
+        double Circlecircum = CircleCircumference.getCircleCircumference(7);
+        double ans  = 7*2*3.1415;
+        assertEquals(Circlecircum, ans,0);
     }
 }
