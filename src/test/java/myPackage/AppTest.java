ce20d227583b1099047e223d99fe12dc4cdf3d21
Alyzahra <alyhesham9864@gmail.com>
2019-11-25 02:46:04 +0200

    Sum test

--- src/test/java/myPackage/AppTest.java
+++ src/test/java/myPackage/AppTest.java
@@ -25,4 +25,11 @@ public class AppTest
         double ans  = 7*6*5*4*3*2*1;
         assertEquals(fact, ans,0);
     }
+    @Test
+    public void TestSum()
+    {
+        double sum = Sum.getSum(7);
+        double ans  = 0+1+2+3+4+5+6+7;
+        assertEquals(sum, ans,0);
+    }
 }
